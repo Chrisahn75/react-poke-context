8ed59807821e6776b49f99e5a40135347cc79fb2
Chrisahn75 <chris.ahona@gmail.com>
2022-02-21 16:44:29 +0100

    end home

--- poke-context/src/components/Home.js
+++ poke-context/src/components/Home.js
@@ -1,8 +1,8 @@
 import React, { useState,useEffect } from 'react';
 
 export default function Home() {
-
   const [pokemon, setPokemon] = useState([]);
+  const [randomNumber, setRandomNumber] = useState([]);
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon/1")
@@ -11,11 +11,23 @@ export default function Home() {
       .catch((err) => console.log(err));
   }, []);
 
+  useEffect(() => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+        .then((res) => res.json())
+        .then((res) => setPokemon(res))
+        .catch((err) => console.log(err));
+  }, [randomNumber]);
+
+  function ChangeRandomNumber() {
+    setRandomNumber(Math.floor(Math.random() * 100) + 1);
+  }
+
   return (
     <>
+      <button onClick={ChangeRandomNumber}>Click to get random Pokemon</button>
       <p>Name : {pokemon.name}</p>
       <p>Height :{pokemon.height}</p>
       <p>Weight : {pokemon.weight}</p>
     </>
   );
-  }
+}
