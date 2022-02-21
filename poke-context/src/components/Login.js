9763b1389faf92ad1825064e31fc750c38ed21e5
Chrisahn75 <chris.ahona@gmail.com>
2022-02-21 15:07:53 +0100

    login part

--- poke-context/src/components/Login.js
+++ poke-context/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useForm } from "react-hook-form";
 
 export default function Login() {
@@ -14,7 +14,7 @@ export default function Login() {
       <h1>Login</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <input {...register("username", { required: true, maxLength:15 })} />
-        {errors.username && <span>Username is required.</span>}
+        {errors.username && <span>Please enter a username .</span>}
         <input {...register("password", { required: true, maxLength:6 })} />
         {errors.password && <span>Please enter a password.</span>}
         <input type="submit" />
