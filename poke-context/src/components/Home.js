bc5cb0168f400748540847d98624d40ac5389606
Chrisahn75 <chris.ahona@gmail.com>
2022-02-21 15:42:05 +0100

    debut home

--- poke-context/src/components/Home.js
+++ poke-context/src/components/Home.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+
 export default function Home() {
-    return (
-      <>
-        <p>Home</p>
-      </>
-    );
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/1")
+      .then((res) => res.json())
+      .then((res) => setPokemon(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <p>Name : {pokemon.name}</p>
+      <p>Height :{pokemon.height}</p>
+      <p>Weight : {pokemon.weight}</p>
+    </>
+  );
   }
